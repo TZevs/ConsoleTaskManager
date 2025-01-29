d519e364d2707515a06806addf0a30bfef5f5558
Thalia <148969711+TZevs@users.noreply.github.com>
2025-01-29 14:55:06 +0000

    base files

---
+++ ToDoListManager/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListManager
+{
+    internal class Tasks
+    {
+        private string taskTitle;
+        private string taskPriority;
+        private DateTime? dueDateTime;
+        private List<string>? subTasks;
+        private string taskStatus;
+    }
+}
